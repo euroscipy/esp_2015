d5032b139dccef63d6025a6db76ed54fceabbf56
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-03-10 15:38:52 +0100

    set relative path for activate_this and PROJECT_ROOT

--- esp_2015/wsgi.py
+++ esp_2015/wsgi.py
@@ -8,6 +8,15 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+VENV_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+sys.path.append(PROJECT_ROOT)
+
+activate_this = os.path.join(VENV_ROOT, 'bin', 'activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "esp_2015.settings")
 
