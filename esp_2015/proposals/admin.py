634153e9517a0c161d4109b1993947bfe14614ec
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-08-04 12:11:13 +0200

    add poster proposals in the admin interface

--- esp_2015/proposals/admin.py
+++ esp_2015/proposals/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import TalkProposal, TutorialProposal
+from .models import TalkProposal, PosterProposal, TutorialProposal
 
 
 admin.site.register(TalkProposal)
+admin.site.register(PosterProposal)
 admin.site.register(TutorialProposal)
